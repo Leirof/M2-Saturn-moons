9340986b13b414d8c2eaf1a135f86237fcb756d0
Vincent (Leirof) <vince.lrf@gmail.com>
2023-01-26 16:05:59 +0100

    Q5 & 6

--- src/plot.py
+++ src/plot.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def pos_evol(bodies, T, DT):
+def pos_dyn(bodies, T, DT):
     fig = plt.figure(figsize=(15,5))
 
     # Evolution of the x coordinate
     plt.subplot(131)
     plt.title('Evolution of the x coordinate')
     for body in bodies:
-        plt.plot(np.arange(T)*DT/86400.0, body.pos_evol[:,0], label=body.name, c=body.color)
+        plt.plot(np.arange(T)*DT/86400.0, body.pos_dyn[:,0], label=body.name, c=body.color)
     plt.xlabel('Time [days]')
     plt.ylabel('x [km]')
     plt.legend()
@@ -17,7 +17,7 @@ def pos_evol(bodies, T, DT):
     plt.subplot(132)
     plt.title('Evolution of the y coordinate')
     for body in bodies:
-        plt.plot(np.arange(T)*DT/86400.0, body.pos_evol[:,1], label=body.name, c=body.color)
+        plt.plot(np.arange(T)*DT/86400.0, body.pos_dyn[:,1], label=body.name, c=body.color)
     plt.xlabel('Time [days]')
     plt.ylabel('y [km]')
     plt.legend()
@@ -26,27 +26,30 @@ def pos_evol(bodies, T, DT):
     plt.subplot(133)
     plt.title('Evolution of the z coordinate')
     for body in bodies:
-        plt.plot(np.arange(T)*DT/86400.0, body.pos_evol[:,2], label=body.name, c=body.color)
+        plt.plot(np.arange(T)*DT/86400.0, body.pos_dyn[:,2], label=body.name, c=body.color)
     plt.xlabel('Time [days]')
     plt.ylabel('z [km]')
     plt.legend()
     plt.show()
 
 def orbits_3D(bodies):
-    # fig = plt.figure(figsize=(5,5))
+    fig = plt.figure(figsize=(5,5))
 
-    # ax = plt.subplot(111, projection='3d')
-    # ax.set_title('Orbits of the bodies')
-    # for body in bodies:
-    #     ax.plot(body.pos_evol[:,0], body.pos_evol[:,1], body.pos_evol[:,2], marker='o', label=body.name, c=body.color)
+    ax = plt.subplot(111, projection='3d')
+    ax.set_title('Orbits of the bodies')
+    for body in bodies:
+        ax.plot(body.pos_dyn[:,0], body.pos_dyn[:,1], body.pos_dyn[:,2], marker='o', label=body.name, c=body.color)
+    
+    plt.show()
 
+def orbits(bodies):
     plt.figure(figsize=(15,5))
 
     # Orbit on plane x,y
     plt.subplot(131)
     plt.title('Orbit on plane x,y')
     for body in bodies:
-        plt.plot(body.pos_evol[:,0], body.pos_evol[:,1], label=body.name, c=body.color) 
+        plt.plot(body.pos_dyn[:,0], body.pos_dyn[:,1], label=body.name, c=body.color) 
     plt.xlabel('x [km]')
     plt.ylabel('y [km]')
     plt.legend()
@@ -55,7 +58,7 @@ def orbits_3D(bodies):
     plt.subplot(132)
     plt.title('Orbit on plane x,z') 
     for body in bodies:
-        plt.plot(body.pos_evol[:,0], body.pos_evol[:,2], label=body.name, c=body.color)
+        plt.plot(body.pos_dyn[:,0], body.pos_dyn[:,2], label=body.name, c=body.color)
     plt.xlabel('x [km]')
     plt.ylabel('z [km]')    
     plt.legend()
@@ -65,21 +68,21 @@ def orbits_3D(bodies):
 
     plt.title('Orbit on plane y,z')
     for body in bodies:
-        plt.plot(body.pos_evol[:,1], body.pos_evol[:,2], label=body.name, c=body.color)
+        plt.plot(body.pos_dyn[:,1], body.pos_dyn[:,2], label=body.name, c=body.color)
     plt.xlabel('y [km]')
     plt.ylabel('z [km]')
     plt.legend()
    
     plt.show()
 
-def vel_evol(bodies, T, DT):
+def vel_dyn(bodies, T, DT):
     fig = plt.figure(figsize=(15,5))
 
     # Evolution of the x velocity
     plt.subplot(131)
     plt.title('Evolution of the x velocity')
     for body in bodies:
-        plt.plot(np.arange(T)*DT/86400.0, body.vel_evol[:,0], label=body.name, c=body.color)
+        plt.plot(np.arange(T)*DT/86400.0, body.vel_dyn[:,0], label=body.name, c=body.color)
     plt.xlabel('Time [days]')
     plt.ylabel('Vx [km/s]')
     plt.legend()
@@ -88,7 +91,7 @@ def vel_evol(bodies, T, DT):
     plt.subplot(132)
     plt.title('Evolution of the y velocity')
     for body in bodies:
-        plt.plot(np.arange(T)*DT/86400.0, body.vel_evol[:,1], label=body.name, c=body.color)
+        plt.plot(np.arange(T)*DT/86400.0, body.vel_dyn[:,1], label=body.name, c=body.color)
     plt.xlabel('Time [days]')
     plt.ylabel('Vy [km/s]')
     plt.legend()
@@ -97,7 +100,7 @@ def vel_evol(bodies, T, DT):
     plt.subplot(133)
     plt.title('Evolution of the z velocity')
     for body in bodies:
-        plt.plot(np.arange(T)*DT/86400.0, body.vel_evol[:,2], label=body.name, c=body.color)
+        plt.plot(np.arange(T)*DT/86400.0, body.vel_dyn[:,2], label=body.name, c=body.color)
     plt.xlabel('Time [days]')
     plt.ylabel('Vz [km/s]')
     plt.legend()
@@ -108,42 +111,42 @@ def orbital_elements(bodies, T, DT):
     
     for i, body in enumerate(bodies):
         plt.subplot(len(bodies),6,i*6+1)
-        plt.plot(np.arange(T)*DT/86400.0, body.orb_evol[:,0], label=body.name, c=body.color)
+        plt.plot(np.arange(T)*DT/86400.0, body.orb_dyn[:,0], label=body.name, c=body.color)
         plt.title('Evolution of the semimajor axis')
         plt.xlabel('Time [days]')
         plt.ylabel('a [km]')
         plt.legend()
 
         plt.subplot(len(bodies),6,i*6+2)
-        plt.plot(np.arange(T)*DT/86400.0, body.orb_evol[:,1], label=body.name, c=body.color)
+        plt.plot(np.arange(T)*DT/86400.0, body.orb_dyn[:,1], label=body.name, c=body.color)
         plt.title('Evolution of the eccentricity')
         plt.xlabel('Time [days]')
         plt.ylabel('e')
         plt.legend()
 
         plt.subplot(len(bodies),6,i*6+3)
-        plt.plot(np.arange(T)*DT/86400.0, body.orb_evol[:,2], label=body.name, c=body.color)
+        plt.plot(np.arange(T)*DT/86400.0, body.orb_dyn[:,2], label=body.name, c=body.color)
         plt.title('Evolution of the inclination')
         plt.xlabel('Time [days]')   
         plt.ylabel('i [deg]')
         plt.legend()
 
         plt.subplot(len(bodies),6,i*6+4)
-        plt.plot(np.arange(T)*DT/86400.0, body.orb_evol[:,3], label=body.name, c=body.color)  
+        plt.plot(np.arange(T)*DT/86400.0, body.orb_dyn[:,3], label=body.name, c=body.color)  
         plt.title('Evolution of the longitude of the ascending node')
         plt.xlabel('Time [days]')
         plt.ylabel(r'$\Omega$ [deg]')   
         plt.legend()    
 
         plt.subplot(len(bodies),6,i*6+5)
-        plt.plot(np.arange(T)*DT/86400.0, body.orb_evol[:,4], label=body.name, c=body.color)
+        plt.plot(np.arange(T)*DT/86400.0, body.orb_dyn[:,4], label=body.name, c=body.color)
         plt.title('Evolution of the argument of periapsis')
         plt.xlabel('Time [days]')
         plt.ylabel(r'$\varpi$ [deg]')   
         plt.legend()
 
         plt.subplot(len(bodies),6,i*6+6)  
-        plt.plot(np.arange(T)*DT/86400.0, body.orb_evol[:,5], label=body.name, c=body.color)
+        plt.plot(np.arange(T)*DT/86400.0, body.orb_dyn[:,5], label=body.name, c=body.color)
         plt.title('Evolution of the mean anomaly')  
         plt.xlabel('Time [days]')
         plt.ylabel(r'$\lambda$ [deg]')
